d9b851147cda23c0b7e0503b4c0cd755fe5437e6
Péter Koppány <koppany.peter@autsoft.hu>
2024-03-10 18:26:02 +0100

    Adding authentication and Initial  ef migration

--- BasicAuth.WebApi/Program.cs
+++ BasicAuth.WebApi/Program.cs
@@ -1,36 +1,64 @@
-using System.Text.Json;
+using BasicAuth.Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<BasicAuthDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString(nameof(BasicAuthDbContext))));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Swagger
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddCors(options => options.AddPolicy(name: "WeatherOrigins",
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+
+// Auth
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+        .AddEntityFrameworkStores<BasicAuthDbContext>();
+
+// CORS
+builder.Services.AddCors(options => options.AddPolicy(name: "ProdOrigins",
     policy =>
     {
         policy.WithOrigins("https://bigmoneybois.hu/").AllowAnyMethod().AllowAnyHeader();
     }));
 
+builder.Services.AddCors(options => options.AddPolicy(name: "DevOrigins",
+    policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    }));
+
 var app = builder.Build();
 
-app.UseCors("WeatherOrigins");
-
-app.Use(async (context, next) =>
-{
-    app.Logger.LogInformation(JsonSerializer.Serialize(context.Request.Headers));
-    Console.WriteLine(JsonSerializer.Serialize(context.Request.Headers));
-    await next.Invoke();
-});
+CreateDbIfNotExists(app);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseCors("DevOrigins");
 }
+else
+{
+    app.UseCors("ProdOrigins");
+}
+
+app.MapIdentityApi<IdentityUser>();
 
 app.UseHttpsRedirection();
 
@@ -39,3 +67,22 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+static void CreateDbIfNotExists(IHost host)
+{
+    using (var scope = host.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        try
+        {
+            var context = services.GetRequiredService<BasicAuthDbContext>();
+            context.Database.EnsureCreated();
+        }
+        catch (Exception ex)
+        {
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "An error occurred creating the DB.");
+        }
+    }
+}
